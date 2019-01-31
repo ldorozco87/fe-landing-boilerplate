f66a23f325c351e1e4606a58c714169d3ad77a51
Diego Orozco <ldorozco@gmail.com>
2019-01-30 22:11:54 -0600

    Initial scaffolding

---
+++ webpack.config.js
@@ -0,0 +1,121 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const path = require('path');
+const isDev = process.env.NODE_ENV !== 'production';
+
+let envPlugins = [];
+if (isDev) {
+    envPlugins = [
+        new webpack.HotModuleReplacementPlugin(),
+    ];
+} else {
+    envPlugins = [
+        new MiniCssExtractPlugin({
+            filename: isDev ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDev ? '[id].css' : '[id].[hash].css',
+        }),
+        new CleanWebpackPlugin(['dist']),
+        new webpack.HashedModuleIdsPlugin(),
+    ];
+}
+
+module.exports = {
+    entry: {
+        app: './src/index.js',
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true,
+    },
+    devtool: 'source-map',
+    mode: isDev ? 'development' : 'production',
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+                    { loader: 'css-loader', options: { sourceMap: true }},
+                    { loader: 'postcss-loader', options: { sourceMap: true }},
+                    { loader: 'sass-loader', options: { sourceMap: true }}
+                ]
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/img'
+                    }
+                }]
+            },
+            {
+                test: /\.(html)$/,
+                include: path.join(__dirname, 'src/partials'),
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                      minimize: !isDev,
+                      interpolate: true,
+                    }
+                }
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            inject: 'body',
+            filename: 'index.html'
+        }),
+        new CopyWebpackPlugin([{ from: 'src/static' }]),
+        ...envPlugins
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            },
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        },
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true,
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    output: {
+        filename: '[name].[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: ''
+    }
+}
